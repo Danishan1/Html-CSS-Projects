3cdaf4b9d04546b3a454c49d30b0b9ad1045ac99
Danishan <danishan089@gmail.com>
2023-10-07 02:21:19 +0530

    update

--- portfolio1/srcipt/changeImage.js
+++ portfolio1/srcipt/changeImage.js
@@ -1,14 +1,14 @@
 const imageElement = document.getElementById("changing-image");
 const imagePaths = [
   "../src/personal/D4.jpg",
-  "../src/personal/D1.jpg",
-  "../src/personal/D2.jpg",
-  "../src/personal/G1.jpg",
-  "../src/personal/G2.jpg",
   "../src/personal/G4.jpg",
+  "../src/personal/D1.jpg",
+  "../src/personal/G7.jpg",
+  "../src/personal/G1.jpg",
   "../src/personal/D5.jpg",
   "../src/personal/G6.jpg",
-  "../src/personal/G7.jpg",
+  "../src/personal/D2.jpg",
+  "../src/personal/G2.jpg",
   "../src/personal/G8.jpg",
 ]; // Add your image URLs here
 
@@ -20,4 +20,4 @@ function changeImage() {
   imageElement.src = newImagePath;
 }
 
-setInterval(changeImage, 5000); // Change image every 5 seconds
+setInterval(changeImage, 5000); // Change image every 10 seconds
