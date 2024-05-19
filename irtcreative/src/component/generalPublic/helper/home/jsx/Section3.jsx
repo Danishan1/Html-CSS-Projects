a6540e1ba70606de14468d6a4a374fbd01c7746c
Danishan <danishan089@gmail.com>
2024-05-19 15:02:29 +0530

    Done: Section 3 for Home Page

--- irtcreative/src/component/generalPublic/helper/home/jsx/Section3.jsx
+++ irtcreative/src/component/generalPublic/helper/home/jsx/Section3.jsx
@@ -1,41 +1,41 @@
 import React from "react";
 import style from "../css/Section3.module.css";
-import CardImg from "../../../../util/jsx/Cards/CardImg";
-import CardSimpleInfo from "../../../../util/jsx/Cards/CardSimpleInfo";
-import CardTextOn from "../../../../util/jsx/Cards/CardTextOn";
-import CardTextSliding from "../../../../util/jsx/Cards/CardTextSliding";
-import img from "../src/item10.jpg";
+import MySection from "./MySection";
+import img1 from "../src/woodWork.jpg";
+import img2 from "../src/creative.jpg";
+import img3 from "../src/bulb1.jpg";
 
 const Section3 = () => {
-  const desc = "My Name is Danishan. You are my Favourate Person";
+  const sections = [
+    {
+      img: img1,
+      heading: "Crafting Experiences, One Design at a Time",
+      desc: "Your space is our canvas, and your style is our inspiration. From the initial concept to the final finishing touches, we're dedicated to creating interiors that resonate with your personality and preferences. With our meticulous attention to detail and unwavering commitment to quality, we'll transform your space into a true reflection of your individuality.",
+    },
+    {
+      img: img2,
+      heading: "Unlock the Potential of Your Space",
+      desc: "Whether it's a cozy home, a bustling office, or a charming restaurant, we understand that every space has its own story to tell. Our team of expert designers is here to listen, collaborate, and guide you through the design process, ensuring that your space not only looks beautiful but also functions seamlessly to meet your needs.",
+    },
+    {
+      img: img3,
+      heading: "Let's Create Something Extraordinary",
+      desc: "Your space, your style, our expertise - together, let's create something extraordinary. From concept to completion, we're here to turn your dreams into reality. Discover the endless possibilities with itsRIGHTtime Creative and embark on a journey to transform your space into a masterpiece that reflects the true essence of who you are.",
+    },
+  ];
 
   return (
-    <>
-      <div className={style.section3}>
-        {/* <CardImg imgPath={img} title={"Danishan"} desc={desc} />
-        <CardSimpleInfo
-          imgPath={img}
-          name={"Salman"}
-          title={"Danishan"}
-          desc={desc}
-          borderCol="red"
+    <div className={style.section3}>
+      {sections.map((section, index) => (
+        <MySection
+          key={index}
+          img={section.img}
+          heading={section.heading}
+          desc={section.desc}
+          textPosition={index}
         />
-        <CardTextOn
-          authorName={"Danishan"}
-          date={"16-06-2000"}
-          imgPath={img}
-          desc={desc}
-        /> */}
-
-        <CardTextSliding
-          imgPath={img}
-          productName={"Danishan"}
-          productID={"12015486"}
-          desc={desc}
-          color="blue"
-        />
-      </div>
-    </>
+      ))}
+    </div>
   );
 };
 
