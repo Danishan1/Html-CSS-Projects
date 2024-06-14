24967941a58607dd002f68edc1603f2f387ebc54
Danishan <danishan089@gmail.com>
2024-06-14 19:47:50 +0530

    Add MeetingBox

--- FullStackProject/client/src/component/Chat/js/ForwardedBox.jsx
+++ FullStackProject/client/src/component/Chat/js/ForwardedBox.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "../css/ForwardedBox.module.css";
+import { MeetingBox } from "./MeetingBox";
 
 export const ForwardedBox = ({ type }) => {
-  return <div className={style.forwardedBox}>{type}</div>;
+  return <div className={style.forwardedBox}><MeetingBox/></div>;
 };
---
+++ FullStackProject/client/src/component/Chat/js/MeetingBox.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import style from "../css/MeetingBox.module.css";
+
+export const MeetingBox = ({ handleData }) => {
+  return <div className={style.meetingBox}>Meeting Box</div>;
+};
