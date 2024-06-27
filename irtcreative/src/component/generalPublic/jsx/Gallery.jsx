8a3fdbbcbb30777022dfaaa6e72fd3a56c73224c
Danishan <danishan089@gmail.com>
2024-06-27 23:01:26 +0530

    update pages to set upcomming updates

--- irtcreative/src/component/generalPublic/jsx/Gallery.jsx
+++ irtcreative/src/component/generalPublic/jsx/Gallery.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import style from "../css/Gallery.module.css";
+import CommingSoon from "./CommingSoon";
 
 const Gallery = () => {
   return (
     <>
-      <div className={style.gallery}>Gallery</div>
+      <div className={style.gallery}>
+        <CommingSoon
+          text={
+            "I am currently developing the 'Gallery' Layout feature. This update will be released shortly."
+          }
+        />
+      </div>
     </>
   );
 };
