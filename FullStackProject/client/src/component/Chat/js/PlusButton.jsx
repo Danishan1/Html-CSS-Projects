3a2082bb2ae33ccde75ee788568e1b1a7c508e7c
Danishan <danishan089@gmail.com>
2024-06-28 19:51:56 +0530

    Update PlusButton : File Upload Error

--- FullStackProject/client/src/component/Chat/js/PlusButton.jsx
+++ FullStackProject/client/src/component/Chat/js/PlusButton.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import IconSetter from "../helper/IconSetter";
 import style from "../css/PlusButton.module.css";
 import {
@@ -26,7 +26,7 @@ const PlusButton = ({ setPlusClickContent }) => {
   const alertRef = useRef(null);
 
   const [alertContainer, setAlertContainer] = useState([]);
-  const [uploadedFile, setUploadedFile] = useState();
+  const [uploadedFile, setUploadedFile] = useState([]);
 
   const handlePlusClick = () => {
     setPlusClickContent();
@@ -46,6 +46,8 @@ const PlusButton = ({ setPlusClickContent }) => {
       handleFileClick(fileInputRef);
     }
   };
+
+  console.log(uploadedFile);
 
   return (
     <div className={style.plusButton}>
@@ -81,7 +83,7 @@ const PlusButton = ({ setPlusClickContent }) => {
         icon={media}
         name={"Media"}
         clickHandle={() => {
-          setPlusClickContent(handleIconClick("media"));
+          // setPlusClickContent(handleIconClick("media"));
           onfileClick("media");
         }}
       />
@@ -89,7 +91,7 @@ const PlusButton = ({ setPlusClickContent }) => {
         icon={fileUpload}
         name={"Document"}
         clickHandle={() => {
-          setPlusClickContent(handleIconClick("document"));
+          // setPlusClickContent(handleIconClick("document"));
           onfileClick("doc");
         }}
       />
@@ -97,7 +99,7 @@ const PlusButton = ({ setPlusClickContent }) => {
         icon={directShare}
         name={"Direct Share"}
         clickHandle={() => {
-          setPlusClickContent(handleIconClick("directShare"));
+          // setPlusClickContent(handleIconClick("directShare"));
           onfileClick("all");
         }}
       />
