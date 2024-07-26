0062260f1f8feaf9d366a4df21c93d9399cb9aea
Danishan <danishan089@gmail.com>
2024-07-26 15:48:49 +0530

    getUserAuth : checking uderiD is present in the db

--- FullStackProject/server/controllers/auth/getUserAuth.js
+++ FullStackProject/server/controllers/auth/getUserAuth.js
@@ -1,10 +1,27 @@
-import generateUserId from "./generateID.js"
+import generateUserId from "./generateID.js";
 import generatePasscode from "./generatePasscode.js";
+import pool from "../../config/db.js";
 
-export const getUserAuth = (req, res) => {
-    const userId = generateUserId();
+export const getUserAuth = async (req, res) => {
+  try {
+    let userId;
+    let isUnique = false;
+
+    // Check if userId is unique, generate a new one if it's not
+    while (!isUnique) {
+      userId = generateUserId();
+      const [rows] = await pool.query('SELECT userId FROM user WHERE userId = ?', [userId]);
+
+      if (rows.length === 0) {
+        isUnique = true;
+      }
+    }
+
     const passcode = generatePasscode();
 
-    res.json({ userId, passcode })
-
-}
+    res.json({ userId, passcode });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
