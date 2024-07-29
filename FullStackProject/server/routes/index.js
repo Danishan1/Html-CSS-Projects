39d8dade74cc87c615418e7a516156aeb5246fb6
Danishan <danishan089@gmail.com>
2024-07-29 10:38:01 +0530

    delete Extra page : msgRoute

--- FullStackProject/server/routes/index.js
+++ FullStackProject/server/routes/index.js
@@ -3,7 +3,6 @@ import authRoutes from './authRoutes.js';
 import userRoutes from './userRoutes.js';
 import testRoutes from './testRoutes.js';
 import chatRoutes from './chatRoutes.js';
-// import messageRoutes from './msgRoute.js';
 
 const router = express.Router();
 
@@ -11,6 +10,5 @@ router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/chats', chatRoutes);
 router.use('/test', testRoutes);
-// router.use('/msg', messageRoutes);
 
 export default router;
