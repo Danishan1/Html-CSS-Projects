06562a3126ecfa9ac578811276d614f382bbd4bf
Danishan <danishan089@gmail.com>
2024-08-10 18:17:59 +0530

    Update : sockit.io Working fine

--- letsDiscuss/client/src/App.js
+++ letsDiscuss/client/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { lazy, useEffect, Suspense, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import axios from 'axios';
+import Loading from './component/SpecialPages/js/Loading';
 
-function App() {
+const LoginForm = lazy(() => import('./component/Login/js/Login'))
+const RegristerForm = lazy(() => import('./component/Registration/js/RegisterForm'))
+const ChatApp = lazy(() => import('./component/Chat/js/ChatApp'))
+const ErrorPage = lazy(() => import('./component/SpecialPages/js/ErrorPage'))
+
+
+
+
+
+const App = () => {
+  const [authenticated, setAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/auth/isAuth', { withCredentials: true });
+
+        if (response.data.isAuth) setAuthenticated(true);
+        else setAuthenticated(false);
+
+      } catch (err) {
+        setAuthenticated(false);
+      } finally {
+        // setTimeout(() => {
+        setLoading(false);
+        // }, 2000); // Simulate a 2 second loading time
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<Loading />}>
+        <Router>
+          <Routes>
+            <Route path="/" element={authenticated ? <Navigate to="/discuss" /> : <Navigate to="/login" />} />
+            <Route path="/login" element={authenticated ? <Navigate to="/discuss" /> : <LoginForm />} />
+            <Route path="/register/*" element={authenticated ? <Navigate to="/discuss" /> : <RegristerForm />} />
+            <Route path="/discuss" element={authenticated ? <ChatApp /> : <Navigate to="/login" />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </Router>
+      </Suspense>
+
+
+
     </div>
   );
-}
+};
 
 export default App;
--- letsDiscuss/server/sockets/socketHandler.js
+++ letsDiscuss/server/sockets/socketHandler.js
@@ -1,7 +1,7 @@
 export const socketHandler = (io, socket) => {
     socket.on('message', (msg) => {
         console.log('Message received:', msg);
-        io.emit('message', msg); // Broadcast the message to all connected clients
+        io.emit('message', "socketHandler"); // Broadcast the message to all connected clients
     });
 
     // Additional event handlers can be added here
