8659835bf0c8fd148f75a8994c1e10185a4c4843
Danishan <danishan089@gmail.com>
2024-08-11 21:49:57 +0530

    updateHandle Connect

--- letsDiscuss/server/sockets/controllers/messageSocket.js
+++ letsDiscuss/server/sockets/controllers/messageSocket.js
@@ -5,3 +5,15 @@ export const messageSocket = (io, socket, msg) => {
     console.log('Session ID:', socket.request.session.id);
     io.emit('message', "socketHandler"); // Broadcast the message to all connected clients
 }
+
+// io.on('connection', (socket) => {
+//     socket.on('joinChat', (chatId) => {
+//         socket.join(chatId);
+//         console.log(`User joined chat room: ${chatId}`);
+//     });
+
+//     socket.on('leaveChat', (chatId) => {
+//         socket.leave(chatId);
+//         console.log(`User left chat room: ${chatId}`);
+//     });
+// });
--- letsDiscuss/server/sockets/handlers/connectionHandler.js
+++ letsDiscuss/server/sockets/handlers/connectionHandler.js
@@ -1,3 +1,21 @@
-export const handleConnect = (io, socket) => {
+import pool from "../../config/db.js";
+
+
+export const handleConnect = async (io, socket) => {
+    const session = socket.request.session;
+    const userId = session.userId;
     console.log('A User Connected', socket.id);
+
+
+    const query = `SELECT DISTINCT chatId FROM chat_list WHERE userId = ?`
+    try {
+        const [userChats] = await pool.query(query, [userId]);
+        let listChatIds = [];
+        userChats.forEach((userChat) => { socket.join(userChat.chatId); listChatIds.push(userChat.chatId); })
+        console.log(`User ${userId} joined chats: ${listChatIds.join(', ')}`);
+
+    } catch (err) {
+        console.log(err);
+    }
+
 };
