ba6a0f5fcaf0f25a996177c9214e6f253b66325b
Danishan <danishan089@gmail.com>
2024-10-24 14:19:50 +0530

    Card Completed

--- CardGame/danishan/src/App.js
+++ CardGame/danishan/src/App.js
@@ -4,7 +4,10 @@ import Card from './components/jsx/Card';
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Card code='DS' setResult={(value) => { console.log(value) }} />
+      <Card code='CS' setResult={(value) => { console.log(value) }} />
+      <Card code='SS' setResult={(value) => { console.log(value) }} />
+      <Card code='HS' setResult={(value) => { console.log(value) }} />
     </div>
   );
 }
--- CardGame/danishan/src/components/jsx/Card.jsx
+++ CardGame/danishan/src/components/jsx/Card.jsx
@@ -1,12 +1,59 @@
 import React from "react";
 import styles from "../css/Card.module.css";
+import gameImage from "../../assets/images/game.png";
+import {
+  clubIcon,
+  diamondIcon,
+  heartIcon,
+  spadeIcon,
+} from "../../assets/js/icons";
 
-const Card = ({ code = "JH" }) => {
+const Card = ({ code = "HJ", setResult }) => {
+  const suitName = code[0];
+  const cardName = code.slice(1);
+
+  let cardIcon;
+
+  switch (suitName) {
+    case "H":
+      cardIcon = heartIcon;
+      break;
+    case "S":
+      cardIcon = spadeIcon;
+      break;
+    case "C":
+      cardIcon = clubIcon;
+      break;
+    case "D":
+      cardIcon = diamondIcon;
+      break;
+    default:
+      cardIcon = null;
+  }
+
+  const cssVariable = {
+    "--suitColor":
+      suitName === "H" || suitName === "D"
+        ? "var(--colorRed)"
+        : "var(--colorGray6)",
+  };
+
   return (
-    <div className={styles.cardContainer}>
+    <div
+      className={styles.cardContainer}
+      style={cssVariable}
+      onClick={() => setResult(code)}
+    >
       <div className={styles.card}>
-        <div className={styles.cardFront}>Front</div>
-        <div className={styles.cardBack}>Back</div>
+        <div className={styles.cardFront}>
+          <div className={styles.content}>
+            <p>{cardName}</p>
+            <p>{cardIcon}</p>
+          </div>
+        </div>
+        <div className={styles.cardBack}>
+          <img src={gameImage} alt="" />
+        </div>
       </div>
     </div>
   );
