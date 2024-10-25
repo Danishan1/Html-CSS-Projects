797dd5dfefaf4e742c07522b1b66189b51e2d8f8
Danishan <danishan089@gmail.com>
2024-10-25 11:40:15 +0530

    Seperate Cards

--- CardGame/danishan/src/components/jsx/CardGame.jsx
+++ CardGame/danishan/src/components/jsx/CardGame.jsx
@@ -2,71 +2,9 @@ import React, { useState, useEffect } from "react";
 import styles from "../css/CardGame.module.css";
 import CardSelection from "./CardSelection";
 import BidStake from "./BidStake";
+import { deckSuits } from "../helper/cards";
 
 export const CardGame = ({ setBid }) => {
-  // Separate the deck into different suits
-  const clubs = [
-    "CA",
-    "C2",
-    "C3",
-    "C4",
-    "C5",
-    "C6",
-    "C7",
-    "C8",
-    "C9",
-    "C10",
-    "CJ",
-    "CQ",
-    "CK",
-  ];
-  const hearts = [
-    "HA",
-    "H2",
-    "H3",
-    "H4",
-    "H5",
-    "H6",
-    "H7",
-    "H8",
-    "H9",
-    "H10",
-    "HJ",
-    "HQ",
-    "HK",
-  ];
-  const diamonds = [
-    "DA",
-    "D2",
-    "D3",
-    "D4",
-    "D5",
-    "D6",
-    "D7",
-    "D8",
-    "D9",
-    "D10",
-    "DJ",
-    "DQ",
-    "DK",
-  ];
-  const spades = [
-    "SA",
-    "S2",
-    "S3",
-    "S4",
-    "S5",
-    "S6",
-    "S7",
-    "S8",
-    "S9",
-    "S10",
-    "SJ",
-    "SQ",
-    "SK",
-  ];
-  const deck = [clubs, diamonds, hearts, spades];
-
   const [timer, setTimer] = useState(10);
   const [selectedCard, setSelectedCard] = useState(null);
   const [screenStage, setScreenStage] = useState(0);
@@ -121,7 +59,7 @@ export const CardGame = ({ setBid }) => {
       <div className={styles.body}>
         {screenStage === 0 ? (
           <CardSelection
-            deck={deck}
+            deck={deckSuits}
             onCardClick={handleCardClick}
             isShow={showCard}
           />
